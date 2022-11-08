3c33f4671ed57b539da1795b7c962bd87a22b1a7
Sylvain Pauchet <sylvain.pauchet@enac.fr>
2022-11-08 18:18:16 +0100

    Add freq buttons

--- code/haptic.py
+++ code/haptic.py
@@ -13,6 +13,14 @@ class Haptic:
         self.fade_out_snd.setSource(QUrl.fromLocalFile('./sounds/fadeOut.wav'))
         self.fade_out_snd.setVolume(1)
 
+        self.freq_asc_sound = QSoundEffect()
+        self.freq_asc_sound.setSource(QUrl.fromLocalFile('./sounds/FreqAsc.wav'))
+        self.freq_asc_sound.setVolume(1)
+
+        self.freq_desc_sound = QSoundEffect()
+        self.freq_desc_sound.setSource(QUrl.fromLocalFile('./sounds/FreqDesc.wav'))
+        self.freq_desc_sound.setVolume(1)
+
         self.beep_snd = QSoundEffect()
         self.beep_snd.setSource(QUrl.fromLocalFile('./sounds/beep.wav'))
         self.beep_snd.setVolume(1)
@@ -22,6 +30,13 @@ class Haptic:
         self.cont_snd.setVolume(0)
         self.cont_snd.setLoopCount(QSoundEffect.Infinite)
 
+    
+    def freq_desc(self):
+        self.freq_desc_sound.play()
+
+    def freq_asc(self):
+        self.freq_asc_sound.play()
+    
     def fade_in(self):
         self.fade_in_snd.play()
 
@@ -49,6 +64,8 @@ if __name__ == "__main__":
 
     start_button = QPushButton("Fade In")
     stp_button = QPushButton("Fade out")
+    freq_asc_button = QPushButton("Freq Asc")
+    freq_desc_button = QPushButton("Freq Desc")
     beep_button = QPushButton("Beep")
     cont_toggle = QCheckBox("Continuous")
     cont_slider = QSlider()
@@ -58,10 +75,14 @@ if __name__ == "__main__":
     widget.setLayout(layout)
     layout.addWidget(start_button)
     layout.addWidget(stp_button)
+    layout.addWidget(freq_asc_button)
+    layout.addWidget(freq_desc_button)
     layout.addWidget(beep_button)
     layout.addWidget(cont_toggle)
     layout.addWidget(cont_slider)
 
+    freq_asc_button.clicked.connect(haptic.freq_asc)
+    freq_desc_button.clicked.connect(haptic.freq_desc)
     start_button.clicked.connect(haptic.fade_in)
     stp_button.clicked.connect(haptic.fade_out)
     beep_button.clicked.connect(haptic.beep)
